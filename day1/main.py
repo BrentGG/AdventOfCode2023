8aef5354d282f38308b08b7aafa2ae433236519a
Brent Gerets <brent.gerets@student.uhasselt.be>
2023-12-05 11:48:00 +0100

    day 1 part 2 completed

--- day1/main.py
+++ day1/main.py
@@ -1,15 +1,26 @@
 if __name__ == '__main__':
     inputFile = open("input.txt", "r")
+    digits = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
 
     calValSum = 0
     for line in inputFile:
         val = "aa"
-        for i in range(len(line)):
-            if not val[0].isdigit() and line[i].isdigit():
-                val = line[i] + val[1]
-            if not val[1].isdigit() and line[len(line) - 1 - i].isdigit():
-                val = val[0] + line[len(line) - 1 - i]
-            if val.isnumeric():
-                calValSum += int(val)
-                break
+        indices = [-1, -1]
+        for idx, char in enumerate(line):
+            if char.isdigit() and (idx < indices[0] or indices[0] < 0):
+                indices[0] = idx
+                val = char + val[1]
+            if char.isdigit() and (idx > indices[1] or indices[1] < 0):
+                indices[1] = idx
+                val = val[0] + char
+        for idx, digit in enumerate(digits):
+            found = line.find(digit)
+            if found >= 0 and (indices[0] < 0 or found < indices[0]):
+                indices[0] = found
+                val = str(idx + 1) + val[1]
+            found = line.rfind(digit)
+            if found >= 0 and (indices[1] < 0 or found > indices[1]):
+                indices[1] = found
+                val = val[0] + str(idx + 1)
+        calValSum += int(val)
     print(calValSum)
