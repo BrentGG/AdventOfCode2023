8b14ec86d59295f3f630ea39ebccfde1a19147af
Brent Gerets <brent.gerets@student.uhasselt.be>
2023-12-05 15:09:13 +0100

    day 2 part 2 completed

--- day2/main.py
+++ day2/main.py
@@ -1,4 +1,4 @@
-if __name__ == '__main__':
+def part1():
     inputFile = open("input.txt", "r")
 
     colors = ["red", "green", "blue"]
@@ -25,3 +25,29 @@ if __name__ == '__main__':
         if possible:
             indexSum += gameIdx
     print(indexSum)
+
+
+def part2():
+    inputFile = open("input.txt", "r")
+
+    colors = ["red", "green", "blue"]
+
+    powerSum = 0
+    for line in inputFile:
+        maximum = [0, 0, 0]
+        splitByColon = line.split(":")
+        splitByColon[1] = splitByColon[1].replace(",", "")
+        splitByColon[1] = splitByColon[1].replace("\n", "")
+        for roundOfGame in splitByColon[1].split(";"):
+            cubes = roundOfGame[1:].split(" ")
+            for i in range(0, len(cubes), 2):
+                for idx, color in enumerate(colors):
+                    if color == cubes[i + 1]:
+                        if int(cubes[i]) > maximum[idx]:
+                            maximum[idx] = int(cubes[i])
+        powerSum += maximum[0] * maximum[1] * maximum[2]
+    print(powerSum)
+
+
+if __name__ == '__main__':
+    part2()
