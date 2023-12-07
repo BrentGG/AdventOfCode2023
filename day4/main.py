b0c5359d4f039dc7885e71bff7832cc17c1122ed
Brent Gerets <brent.gerets@student.uhasselt.be>
2023-12-07 14:11:19 +0100

    day 4 part 2 completed

--- day4/main.py
+++ day4/main.py
@@ -15,5 +15,29 @@ def part1():
     print(point_total)
 
 
+def get_count(idx, matching_amount):
+    count = 0
+    for i in range(1, matching_amount[idx] + 1):
+        if idx + i < len(matching_amount):
+            count += get_count(idx + i, matching_amount)
+    return count + 1
+
+
+def part2():
+    input_file = open("input.txt", "r")
+
+    matching_amounts = []
+    for line in input_file:
+        line_split = line.replace("  ", " ").replace("\n", "").split(":")[1].split("|")
+        winning_numbers = [int(x) for x in line_split[0].split(" ") if x != '']
+        your_numbers = [int(x) for x in line_split[1].split(" ") if x != '']
+        matching_amounts.append(len(set(winning_numbers) & set(your_numbers)))
+
+    count = 0
+    for idx in range(len(matching_amounts)):
+        count += get_count(idx, matching_amounts)
+    print(count)
+
+
 if __name__ == '__main__':
-    part1()
+    part2()
