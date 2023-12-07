e75d65739670308dbb2879e7fdd93cc1014e2632
Brent Gerets <brent.gerets@student.uhasselt.be>
2023-12-07 13:33:00 +0100

    day 3 part 2 completed

--- day3/main.py
+++ day3/main.py
@@ -33,5 +33,36 @@ def part1():
     print(partNumberSum)
 
 
+def part2():
+    input_file = open("input.txt", "r")
+
+    schematic = []
+    for line in input_file:
+        schematic.append(line.replace("\n", ""))
+
+    gearRatioSum = 0
+    for row, line in enumerate(schematic):
+        for col, char in enumerate(line):
+            if char == '*':
+                usedIdxs = []
+                partNumbers = []
+                for i in range(col - 1, col + 2):
+                    for j in range(row - 1, row + 2):
+                        if 0 <= i < len(schematic[0]) and 0 <= j < len(schematic) and schematic[j][i].isdigit() and not usedIdxs.__contains__([j, i]):
+                            numberIdx = i
+                            while numberIdx >= 0 and schematic[j][numberIdx].isdigit():
+                                numberIdx -= 1
+                            numberIdx += 1
+                            number = ""
+                            while numberIdx < len(schematic[0]) and schematic[j][numberIdx].isdigit():
+                                usedIdxs.append([j, numberIdx])
+                                number += schematic[j][numberIdx]
+                                numberIdx += 1
+                            partNumbers.append(int(number))
+                if len(partNumbers) == 2:
+                    gearRatioSum += partNumbers[0] * partNumbers[1]
+    print(gearRatioSum)
+
+
 if __name__ == '__main__':
-    part1()
+    part2()
