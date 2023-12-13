ee1a20f8369a3d0eb78323be9b0c5095f4686501
Brent Gerets <brent.gerets@student.uhasselt.be>
2023-12-13 13:03:39 +0100

    day 6 part 2 completed

--- day6/main.py
+++ day6/main.py
@@ -33,5 +33,39 @@ def part1():
     print(product)
 
 
+def binary_search(start, end, time, dist, first_half):
+    if end - start == 1:
+        if first_half:
+            if (time - start) * start > dist:
+                return start
+            return end
+        else:
+            if (time - end) * end > dist:
+                return end
+            return end
+
+    hold_time = start + int(((end - start) / 2))
+    if ((time - hold_time) * hold_time > dist and first_half) or ((time - hold_time) * hold_time <= dist and not first_half):
+        return binary_search(start, hold_time, time, dist, first_half)
+    else:
+        return binary_search(hold_time, end, time, dist, first_half)
+
+
+def part2():
+    input_file = open("input.txt", "r")
+
+    time = int(input_file.readline().replace("\n", "").split(":")[1].replace(" ", ""))
+    dist = int(input_file.readline().replace("\n", "").split(":")[1].replace(" ", ""))
+
+    hold_time_min = int(time / 2)
+    hold_time_max = hold_time_min
+    if time % 2 != 0:
+        hold_time_max += 1
+
+    hold_time_min = binary_search(0, hold_time_min, time, dist, True)
+    hold_time_max = binary_search(hold_time_max, time, time, dist, False)
+    print(hold_time_max - hold_time_min)
+
+
 if __name__ == "__main__":
-    part1()
+    part2()
