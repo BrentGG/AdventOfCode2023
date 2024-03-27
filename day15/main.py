45c1a4f4f15757118a427f28822da509e78ffd6b
Brent Gerets <brent.gerets@student.uhasselt.be>
2024-03-27 08:47:55 +0100

    day 15 part 2 completed

--- day15/main.py
+++ day15/main.py
@@ -11,9 +11,49 @@ def part1():
         total += value
     print(total)
 
+
+class Lens():
+    def __init__(self, label, focal_length):
+        self.label = label
+        self.focal_length = focal_length
+
+def find_lens(box, label):
+    for idx, lens in enumerate(box):
+        if lens.label == label:
+            return idx
+    return -1
+
 def part2():
-    pass
+    seq = open("input.txt", "r").read().replace("\n", "").split(",")
+
+    boxes = [[] for i in range(256)]
+    for step in seq:
+        box = 0
+        operator_idx = step.find("=")
+        if operator_idx < 0:
+            operator_idx = step.find("-")
+        label = step[:operator_idx]
+        for char in label:
+            box += ord(char)
+            box *= 17
+            box %= 256
+        if step[operator_idx] == "=":
+            idx = find_lens(boxes[box], label)
+            if idx >= 0:
+                boxes[box][idx].focal_length = int(step[-1])
+            else:
+                boxes[box].append(Lens(label, int(step[-1])))
+        elif step[operator_idx] == "-":
+            idx = find_lens(boxes[box], label)
+            if idx >= 0:
+                del boxes[box][idx]
+
+    total = 0
+    for box_idx, box in enumerate(boxes):
+        for slot_idx, lens in enumerate(box):
+            total += (box_idx + 1) * (slot_idx + 1) * lens.focal_length
+    print(total)
 
 
 if __name__ == "__main__":
-    part1()
+    part2()
